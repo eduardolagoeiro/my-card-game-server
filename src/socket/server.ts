860d4cc6b82399c069dbdd4e3a4bd71b2b4ef838
eduardolagoeiro <eduardolagoeiro@gmail.com>
2021-06-19 10:28:20 -0300

    feat: better socket server logs

--- src/socket/server.ts
+++ src/socket/server.ts
@@ -1,9 +1,13 @@
 import io from './singleton';
 import handlers from './handlers';
+import debug from 'debug';
+
+const log = debug('socket:server');
 
 io.on('connection', (socket) => {
+  log('connected', socket.id);
   socket.onAny((listener, data) => {
-    // console.log({ listener, data });
+    log({ listener, data });
     if (typeof data !== 'object') {
       throw new Error('DataNotObject');
     }
